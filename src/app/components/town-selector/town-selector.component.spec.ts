3de095feb596d0b27a45deb698831ec873fc784b
Louis Fredice NJAKO MOLOM <njakolouis@gmail.com>
2024-10-28 16:05:59 +0100

    test: check content of Available Towns list

--- src/app/components/town-selector/town-selector.component.spec.ts
+++ src/app/components/town-selector/town-selector.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TownSelectorComponent } from './town-selector.component';
 import {provideRouter} from '@angular/router';
 import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
-import {screen} from '@testing-library/angular';
+import {screen, getAllByRole} from '@testing-library/angular';
+import {TownWeather} from '@models/town-weather';
+import {MOCK_TONWS_WEATHER} from '../../mocks/wheather.mock';
 
 describe('TownSelectorComponent', () => {
   let component: TownSelectorComponent;
@@ -33,5 +34,16 @@ describe('TownSelectorComponent', () => {
   it('should contains Available Towns list', () => {
     expect(component).toBeTruthy();
     expect(screen.getByRole('list', { name: 'Available Towns' })).toBeTruthy();
+  });
+
+  it('should contains Available Towns list with good content', () => {
+    expect(component).toBeTruthy();
+    component.globalWeather = MOCK_TONWS_WEATHER;
+    fixture.detectChanges();
+    const listOfTownsContainer = screen.getByRole('list', {name: 'Available Towns'});
+    const listItems = getAllByRole(listOfTownsContainer, 'listitem');
+    expect(listItems.length).toEqual(MOCK_TONWS_WEATHER.length);
+    expect(listItems.map(listItem => listItem.textContent))
+      .toEqual(MOCK_TONWS_WEATHER.map(town => town.name));
   });
 });
